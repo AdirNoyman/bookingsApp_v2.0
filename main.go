426c19bef031fb8987688fe7dc80ddef00f4f6e0
Adir Noyman <45500652+AdirNoyman@users.noreply.github.com>
2022-07-31 23:01:03 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -1,25 +1,70 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
 
+const portNumber = ":8080"
+
+func Home(w http.ResponseWriter, r *http.Request) {
+
+	_, err := fmt.Fprintf(w, "<h1>This is the home page 🤓</h1>")
+	if err != nil {
+		println(err)
+
+	}
+
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(2, 2)
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("<h1>This is the about page 😎\nAnd 2 + 2 = %d</h1>", sum))
+
+}
+
+func addValues(x, y int) int {
+
+	return x + y
+
+}
+
+func Divide(w http.ResponseWriter, r *http.Request) {
+
+	x, y := 100.0, 20.0
+	f, err := DivideValues(100.0, 20.0)
+	if err != nil {
+
+		fmt.Fprintf(w, "Can't devide by zero 😩")
+		return
+	}
+
+	_, err = fmt.Fprintf(w, fmt.Sprintf("<h1>%f divided by %f is %f</h1>", x, y, f))
+	if err != nil {
+		println(err)
+	}
+
+}
+
+func DivideValues(x, y float32) (float32, error) {
+
+	if y <= 0 {
+		err := errors.New("hey ASSHOLE 🤨 - can't devide by zero")
+		return 0, err
+	}
+	result := x / y
+
+	return result, nil
+}
+
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/divide", Divide)
+	http.HandleFunc("/about", About)
 
-		n, err := fmt.Fprintf(w, "<h1>Hello Adiros 🤓</h1>")
-
-		if err != nil {
-
-			fmt.Println("Error:", err)
-		}
-
-		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
-
-	})
-
-	_ = http.ListenAndServe(":3000", nil)
+	fmt.Println(fmt.Sprintf("Server started. Listening on port %s 🤓🤟", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
 
 }
